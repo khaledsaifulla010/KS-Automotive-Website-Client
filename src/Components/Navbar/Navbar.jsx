123b6ac9b4b75645adc7fe14c88de2bcd02a02da
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-02-19 10:58:01 +0600

    added time & date in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,28 @@
 import { Link } from 'react-router-dom';
 import companyLogo from '../../assets/image/1.Company-Logo.jpg'
+import moment from 'moment';
+
 
 const Navbar = () => {
+    const date = moment().format('Do MMMM  YYYY');
+    const time = moment().format('LTS');
+
     return (
         <div className='m-8'>
             <div className="navbar bg-slate-100 h-28 rounded-xl">
                 <div className="navbar-start">
-                    <div className='gap-4 flex items-center'>
-                        <img className='lg:w-16 rounded-xl lg:h-10' src={companyLogo} alt="" />
-                        <h2 className='lg:text-3xl font-bold'>KS AUTOMOTIVE LIMITED</h2>
+                    <div className='grid mt-4'>
+                        <div className='gap-4 flex items-center'>
+                            <img className='lg:w-16 rounded-xl lg:h-10' src={companyLogo} alt="" />
+                            <h2 className='lg:text-3xl font-bold'>KS AUTOMOTIVE LIMITED</h2>
+
+                        </div>
+                        <div className='flex items-center gap-4 mt-2 font-bold'>
+                            <h2>Time: {time} </h2>
+                            <h2>Date: {date} </h2>
+                        </div>
                     </div>
+
                 </div>
                 <div className="navbar-end hidden lg:flex">
                     <ul className="menu menu-horizontal px-1 gap-x-4">
@@ -18,9 +31,10 @@ const Navbar = () => {
                         <Link className='bg-gradient-to-r from-purple-500 to-pink-500 rounded-lg font-bold text-lg text-white ' to={'myCart'}><li><a>My Cart</a></li></Link>
                         <Link className='bg-gradient-to-r from-violet-500 to-fuchsia-500 rounded-lg font-bold text-lg text-white' to={'logIn'}><li><a>Login/Register</a></li></Link>
 
-
                     </ul>
                 </div>
+
+
             </div>
         </div>
     );
